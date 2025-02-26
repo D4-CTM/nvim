1e3771ac9827210769caa38964c4ec948088117a
D4-CTM <josuedelcid325@gmail.com>
2025-02-25 20:10:59 -0600

    rework to save on clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -24,6 +24,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "Y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
