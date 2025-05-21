02b487e16d38cc912d68d7f1ab3b853087a663ba
D4-CTM <josuedelcid325@gmail.com>
2025-05-21 11:05:33 -0600

    The autostart was giving issues on the lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,31 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
     {
         "mfussenegger/nvim-jdtls"
     },
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"clangd",
-                    "omnisharp",
-					"css_variables",
-					"gopls",
-					"html",
-					"jdtls",
-					"sqls",
-					"ts_ls",
-					"zls",
-                    "pylsp"
-				},
-				automatic_installation = true,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            --    require("mason-lspconfig").setup({
+            -- automatic_installation = true,
+            --   })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.css_variables.setup({ capabilities = capabilities })
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.clangd.setup({ capabilities = capabilities })
+            lspconfig.css_variables.setup({ capabilities = capabilities })
             lspconfig.omnisharp.setup({
                 cmd = {
                     "dotnet",
@@ -46,30 +33,53 @@ return {
                 },
                 capabilities = capabilities
             })
-			lspconfig.gopls.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.jdtls.setup({ capabilities = capabilities })
-			lspconfig.sqls.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
-			lspconfig.pylsp.setup({ capabilities = capabilities })
+            lspconfig.gopls.setup({ capabilities = capabilities })
+            lspconfig.html.setup({ capabilities = capabilities })
+            lspconfig.jdtls.setup({ capabilities = capabilities })
+            lspconfig.sqls.setup({ capabilities = capabilities })
+            lspconfig.ts_ls.setup({ capabilities = capabilities })
+            lspconfig.pylsp.setup({ capabilities = capabilities })
             lspconfig.zls.setup({
                 capabilities = capabilities,
-                on_attach = function(client,  _)
-                  client.server_capabilities.semanticTokensProvider = nil
+                on_attach = function(client, _)
+                    client.server_capabilities.semanticTokensProvider = nil
                 end
+            })
+            lspconfig.angularls.setup({
+                cmd = {
+                    "ngserver",
+                    "--stdio",
+                    "--tsProbeLocations",
+                    vim.fn.getcwd(),
+                    "--ngProbeLocations",
+                    vim.fn.getcwd(),
+                },
+                on_new_config = function(new_config, _)
+                    new_config.cmd = {
+                        "ngserver",
+                        "--stdio",
+                        "--tsProbeLocations",
+                        vim.fn.getcwd(),
+                        "--ngProbeLocations",
+                        vim.fn.getcwd(),
+                    }
+                end,
+                filetypes = { "typescript", "html" },
+                root_dir = lspconfig.util.root_pattern("angular.json", "project.json"),
+                capabilities = capabilities,
             })
 
             vim.diagnostic.config({
-              virtual_text = true,  -- show inline errors
-              signs = true,         -- show signs in the gutter
-              underline = true,     -- underline problem areas
-              update_in_insert = false,  -- don't update while typing
-              severity_sort = true, -- sort by severity
+                virtual_text = true,      -- show inline errors
+                signs = true,             -- show signs in the gutter
+                underline = true,         -- underline problem areas
+                update_in_insert = false, -- don't update while typing
+                severity_sort = true,     -- sort by severity
             })
 
-			vim.keymap.set("n", "?", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.keymap.set("n", "?", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
